b0f6fd08ab523e8bf3892f9230f4f4d6027b2f2c
Víctor Romero <61208536+Zuam7@users.noreply.github.com>
2022-04-23 18:44:50 -0500

    Add files via upload

---
+++ routes/crear_cuenta.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import ToHomeNav from "../components/ToHomeNavbar.js"
+import Footer from "../components/Footer.js"
+
+export default function Login() {
+    return (
+        <div>
+            <ToHomeNav />
+
+
+
+            <Footer />
+        </div>
+    )
+}
